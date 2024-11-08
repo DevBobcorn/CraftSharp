ed3ffb9b52d5f131af704f55cd1c5864f84fe9e1
Tasuku Bobcorn <DevBobcorn@outlook.com>
2024-11-08 12:48:41 +0800

    :sparkles: Add more item types

--- Runtime/Scripts/Inventory/ItemActionType.cs
+++ Runtime/Scripts/Inventory/ItemActionType.cs
@@ -2,41 +2,50 @@
 {
     public enum ItemActionType
     {
-        None, // Food actions are handled separately, for most food items the action type is none
+        // Food actions are handled separately, for most food items
+        // the action type is none, some others are placeable as blocks
+        None,
 
         // Place blocks
         Block,
-        Lighter,      // Fire block
-        FilledBucket, // Liquid source block
+        Lighter,         // Flint & steel / FireCharge
+        SolidBucket,     // Powder snow, etc. Added in 1.17
+        FluidBucket,     // Water / Lava
 
-        // Dig/attack/defend tools
-        Shears,
-        Axe,
-        Pickaxe,
-        Shovel,
-        Hoe,
+        // Use in hand
         Sword,
         Bow,
         Crossbow,
         Trident,
         Shield,
-
-        // Use in hand
-        DrinkableBottle, // Potions & honey bottle
+        DrinkableBottle, // Potions / Honey bottle
         DrinkableBucket, // Milk bucket
         FoodOnAStick,
         EmptyMap,
         WritableBook,
         WrittenBook,
+        FishingRod,
+        KnowledgeBook,
+        Spyglass,        // Added in 1.17
+        Bundle,          // Added in 1.17
+        Instrument,      // Goat horn, etc. Added in 1.19
 
         // Use on blocks
+        Shears,
+        Axe,
+        Pickaxe,
+        Shovel,
+        Hoe,
         BoneMeal,
-        Honeycomb,   // Waxing in 1.17+
         Record,
         EmptyBottle,
         EmptyBucket,
+        DebugStick,
+        Honeycomb,       // Waxing action added in 1.17+
+        Brush,           // Added 1.19.4
 
         // Use on entities
+        Lead,
         NameTag,
 
         // Spawn entities
@@ -49,11 +58,9 @@
         Boat,
         Minecart,
         EndCrystal,
+        EyeOfEnder,
         EnderPearl,
-        Projectile, // Egg/snowball/experience bottle
-        FishingRod,
-        Lead,
-        KnowledgeBook,
-        DebugStick
+        Projectile,      // Egg / Snowball / Experience bottle
+        MobBucket,       // Fish bucket / Axolotl bucket / Tadpole bucket
     }
 }
