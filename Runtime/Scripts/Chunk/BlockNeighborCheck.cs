206fd977a649c9f82cb7424a51d87e888f499564
Tasuku Bobcorn <devbobcorn@outlook.com>
2026-01-14 00:29:30 +0800

    :boom: Add direction to block neighbor check

--- Runtime/Scripts/Chunk/BlockNeighborCheck.cs
+++ Runtime/Scripts/Chunk/BlockNeighborCheck.cs
@@ -1,15 +1,15 @@
 namespace CraftSharp
 {
-    public delegate bool BlockNeighborCheck(BlockState self, BlockState neighbor);
+    public delegate bool BlockNeighborCheck(BlockState self, BlockState neighbor, Direction direction);
 
     public static class BlockNeighborChecks
     {
-        public static readonly BlockNeighborCheck WATER_SURFACE = (_, neighbor)
-            => !(neighbor.InWater || neighbor.MeshFaceOcclusionSolid);
-        public static readonly BlockNeighborCheck LAVA_SURFACE  = (_, neighbor)
-            => !(neighbor.IsLava  || neighbor.MeshFaceOcclusionSolid);
+        public static readonly BlockNeighborCheck WATER_SURFACE = (_, neighbor, direction)
+            => !(neighbor.InWater || (neighbor.MeshFaceOcclusionSolid && direction != Direction.Up));
+        public static readonly BlockNeighborCheck LAVA_SURFACE  = (_, neighbor, direction)
+            => !(neighbor.IsLava  || (neighbor.MeshFaceOcclusionSolid && direction != Direction.Up));
 
-        public static readonly BlockNeighborCheck NON_FULL_SOLID = (_, neighbor)
+        public static readonly BlockNeighborCheck NON_FULL_SOLID = (_, neighbor, _)
             => !neighbor.MeshFaceOcclusionSolid;
         
     }
